06018d972a93366223e734b0b098e4ce84ccec12
Your Name <you@example.com>
2026-07-06 17:50:56 -0300

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'http://localhost',
+  site: 'https://nandits.github.io/imobiliario',
+  base: '/imobiliario',
   integrations: [tailwind(), sitemap()],
 });
